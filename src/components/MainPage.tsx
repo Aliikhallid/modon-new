57fb1d0c7f5f71a5217b617ea1c093bbe3393d25
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-11-04 23:05:35 +0200

    Added Image Slider

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,12 +5,18 @@ import LastPage from './sections/LastPage'
 import Footer from './sections/Footer'
 import SecondPage from './sections/SecondPage'
 import { useLayoutEffect } from 'react'
+import ImageSlider from './ImageSlider'
 const MainPage = () => {
   useLayoutEffect(() => {
     window.scrollTo(0, 0)
 });
   return (
     <>
+         <div className=' relative  h-screen flex items-center regular mt-10'>
+    <ImageSlider imgUrls={["src/assets/ConstructionUpdates/Bayti/2018/July/0.webp","src/assets/ConstructionUpdates/Bayti/2018/July/1.webp","src/assets/ConstructionUpdates/Bayti/2018/July/2.webp"]}/>
+
+    </div>
+
      <Header />
      <SecondPage />
      <ModonProps />
