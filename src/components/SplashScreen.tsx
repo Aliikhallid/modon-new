fca263cf7e501527dc5fca42249dcc0eb9fc86a2
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2023-11-05 00:03:45 +0200

    Removed Animation and Added Gif

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,43 +1,6 @@
-import React, { useEffect } from 'react';
-import GIF from '../assets/Bayti-logo.webp';
-import anime from 'animejs';
+import GIF from '../assets/SplashGifSample.gif';
 
 export default function SplashScreen() {
-    const animate = () => {
-        const loader = anime.timeline({
-        })
-        loader.add({
-            targets: "#logo",
-            delay: 0,
-            scale: 1,
-            duration: 800,
-            easing: "easeInOutExpo"
-        }).add({
-            targets: "#logo",
-            delay: 0,
-            scale: 1.25,
-            duration: 800,
-            easing: "easeInOutExpo"
-        }).add({
-            targets: "#logo",
-            delay: 0,
-            scale: 1,
-            duration: 800,
-            easing: "easeInOutExpo"
-        }).add({
-            targets: "#logo",
-            delay: 0,
-            scale: 1.25,
-            duration: 800,
-            easing: "easeInOutExpo"
-        })
-        
-    }
-
-  useEffect(() => {
-    console.log("Here")
-    animate(); // Start the animation when the component mounts
-  }, []);
 
   return (
     <div className='w-full h-full bg-white flex flex-col justify-center gap-0 items-center fixed'>
