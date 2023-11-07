54d0d24d8a91818fb8305a1687f407346b3a2d57
Ali_khalid <aliiikhallid@icloud.com>
2023-11-07 22:10:13 +0200

    k

--- src/App.tsx
+++ src/App.tsx
@@ -41,38 +41,38 @@ function App() {
     <>
     <HashRouter>
     <Routes>
-      <Route path='#/' element={<MainPage />}/>
-      <Route path='#/buruj' element={<BurujProject />}/>
-      <Route path='#/buruj/hadeer-towers' element={<HadeerTowers />}/>
-      <Route path='#/buruj/sorouh-towers' element={<SorouhTowers />}/>
-      <Route path='#/lavida' element={<LavidaProject />}/>
-      <Route path='#/bayti' element={<BaytiProject />}/>
-      <Route path='#/bayti/jory' element={<Jory />}/>
-      <Route path='#/bayti/lotus' element={<Lotus />}/>
-      <Route path='#/bayti/narjis' element={<Narjis />}/>
-      <Route path='#/bayti/viola' element={<Viola />}/>
-      <Route path='#/bayti/yasmin' element={<Yasmin />}/>
-      <Route path='#/madinaty' element={<MadinatyProject />}/>
-      <Route path='#/madinaty/cadi' element={<Cadi />}/>
-      <Route path='#/madinaty/gardenia' element={<Gardenia />}/>
-      <Route path='#/madinaty/nargis' element={<Nargis />}/>
-      <Route path='#/madinaty/viola' element={<ViolaM />}/>
-      <Route path='#/madinaty/apartments' element={<MadinatyApartments />}/>
-      <Route path='#/future-city' element={<FutureProject />}/>
-      <Route path='#/news' element={<NewsPage />}/>
-      <Route path='#/contact' element={<ContactPage />}/>
-      <Route path='#/careers' element={<CareersPage />}/>
-      <Route path='#/construction' element={<ConstructionUpdates />}/>
-      <Route path='#/buruj-updates' element={<BurujUpdates />}/>
-      <Route path='#/future-updates' element={<FutureUpdates />}/>
-      <Route path='#/lavida-updates' element={<LavidaUpdates />}/>
-      <Route path='#/madinaty-updates' element={<MadinatyUpdates />}/>
-      <Route path='#/bayti-updates' element={<BaytiUpdates />}/>
-      <Route path='#/commercial' element={<CommercialPage />}/>
-      <Route path='#/bayti-mall' element={<BaytiMall />}/>
-      <Route path='#/about-us' element={<AboutPage />}/>
-      <Route path='#/image-gallery' element={<ImageGallery />}/>
-      <Route path='#/video-gallery' element={<VideoGallery />}/>
+      <Route path='/' element={<MainPage />}/>
+      <Route path='/buruj' element={<BurujProject />}/>
+      <Route path='/buruj/hadeer-towers' element={<HadeerTowers />}/>
+      <Route path='/buruj/sorouh-towers' element={<SorouhTowers />}/>
+      <Route path='/lavida' element={<LavidaProject />}/>
+      <Route path='/bayti' element={<BaytiProject />}/>
+      <Route path='/bayti/jory' element={<Jory />}/>
+      <Route path='/bayti/lotus' element={<Lotus />}/>
+      <Route path='/bayti/narjis' element={<Narjis />}/>
+      <Route path='/bayti/viola' element={<Viola />}/>
+      <Route path='/bayti/yasmin' element={<Yasmin />}/>
+      <Route path='/madinaty' element={<MadinatyProject />}/>
+      <Route path='/madinaty/cadi' element={<Cadi />}/>
+      <Route path='/madinaty/gardenia' element={<Gardenia />}/>
+      <Route path='/madinaty/nargis' element={<Nargis />}/>
+      <Route path='/madinaty/viola' element={<ViolaM />}/>
+      <Route path='/madinaty/apartments' element={<MadinatyApartments />}/>
+      <Route path='/future-city' element={<FutureProject />}/>
+      <Route path='/news' element={<NewsPage />}/>
+      <Route path='/contact' element={<ContactPage />}/>
+      <Route path='/careers' element={<CareersPage />}/>
+      <Route path='/construction' element={<ConstructionUpdates />}/>
+      <Route path='/buruj-updates' element={<BurujUpdates />}/>
+      <Route path='/future-updates' element={<FutureUpdates />}/>
+      <Route path='/lavida-updates' element={<LavidaUpdates />}/>
+      <Route path='/madinaty-updates' element={<MadinatyUpdates />}/>
+      <Route path='/bayti-updates' element={<BaytiUpdates />}/>
+      <Route path='/commercial' element={<CommercialPage />}/>
+      <Route path='/bayti-mall' element={<BaytiMall />}/>
+      <Route path='/about-us' element={<AboutPage />}/>
+      <Route path='/image-gallery' element={<ImageGallery />}/>
+      <Route path='/video-gallery' element={<VideoGallery />}/>
 
     </Routes>
     </HashRouter>
