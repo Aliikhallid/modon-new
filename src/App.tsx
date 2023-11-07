0ba33b7f92e1f3111983481245e2869799ef907a
Ali_khalid <aliiikhallid@icloud.com>
2023-11-07 21:23:09 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -40,38 +40,38 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='modon-new/' element={<MainPage />}/>
-      <Route path='modon-new/buruj' element={<BurujProject />}/>
-      <Route path='modon-new/buruj/hadeer-towers' element={<HadeerTowers />}/>
-      <Route path='modon-new/buruj/sorouh-towers' element={<SorouhTowers />}/>
-      <Route path='modon-new/lavida' element={<LavidaProject />}/>
-      <Route path='modon-new/bayti' element={<BaytiProject />}/>
-      <Route path='modon-new/bayti/jory' element={<Jory />}/>
-      <Route path='modon-new/bayti/lotus' element={<Lotus />}/>
-      <Route path='modon-new/bayti/narjis' element={<Narjis />}/>
-      <Route path='modon-new/bayti/viola' element={<Viola />}/>
-      <Route path='modon-new/bayti/yasmin' element={<Yasmin />}/>
-      <Route path='modon-new/madinaty' element={<MadinatyProject />}/>
-      <Route path='modon-new/madinaty/cadi' element={<Cadi />}/>
-      <Route path='modon-new/madinaty/gardenia' element={<Gardenia />}/>
-      <Route path='modon-new/madinaty/nargis' element={<Nargis />}/>
-      <Route path='modon-new/madinaty/viola' element={<ViolaM />}/>
-      <Route path='modon-new/madinaty/apartments' element={<MadinatyApartments />}/>
-      <Route path='modon-new/future-city' element={<FutureProject />}/>
-      <Route path='modon-new/news' element={<NewsPage />}/>
-      <Route path='modon-new/contact' element={<ContactPage />}/>
-      <Route path='modon-new/careers' element={<CareersPage />}/>
-      <Route path='modon-new/construction' element={<ConstructionUpdates />}/>
-      <Route path='modon-new/buruj-updates' element={<BurujUpdates />}/>
-      <Route path='modon-new/future-updates' element={<FutureUpdates />}/>
-      <Route path='modon-new/lavida-updates' element={<LavidaUpdates />}/>
-      <Route path='modon-new/madinaty-updates' element={<MadinatyUpdates />}/>
-      <Route path='modon-new/bayti-updates' element={<BaytiUpdates />}/>
-      <Route path='modon-new/commercial' element={<CommercialPage />}/>
-      <Route path='modon-new/bayti-mall' element={<BaytiMall />}/>
-      <Route path='modon-new/about-us' element={<AboutPage />}/>
-      <Route path='modon-new/image-gallery' element={<ImageGallery />}/>
-      <Route path='modon-new/video-gallery' element={<VideoGallery />}/>
+      <Route path='/modon-new/' element={<MainPage />}/>
+      <Route path='/modon-new/buruj' element={<BurujProject />}/>
+      <Route path='/modon-new/buruj/hadeer-towers' element={<HadeerTowers />}/>
+      <Route path='/modon-new/buruj/sorouh-towers' element={<SorouhTowers />}/>
+      <Route path='/modon-new/lavida' element={<LavidaProject />}/>
+      <Route path='/modon-new/bayti' element={<BaytiProject />}/>
+      <Route path='/modon-new/bayti/jory' element={<Jory />}/>
+      <Route path='/modon-new/bayti/lotus' element={<Lotus />}/>
+      <Route path='/modon-new/bayti/narjis' element={<Narjis />}/>
+      <Route path='/modon-new/bayti/viola' element={<Viola />}/>
+      <Route path='/modon-new/bayti/yasmin' element={<Yasmin />}/>
+      <Route path='/modon-new/madinaty' element={<MadinatyProject />}/>
+      <Route path='/modon-new/madinaty/cadi' element={<Cadi />}/>
+      <Route path='/modon-new/madinaty/gardenia' element={<Gardenia />}/>
+      <Route path='/modon-new/madinaty/nargis' element={<Nargis />}/>
+      <Route path='/modon-new/madinaty/viola' element={<ViolaM />}/>
+      <Route path='/modon-new/madinaty/apartments' element={<MadinatyApartments />}/>
+      <Route path='/modon-new/future-city' element={<FutureProject />}/>
+      <Route path='/modon-new/news' element={<NewsPage />}/>
+      <Route path='/modon-new/contact' element={<ContactPage />}/>
+      <Route path='/modon-new/careers' element={<CareersPage />}/>
+      <Route path='/modon-new/construction' element={<ConstructionUpdates />}/>
+      <Route path='/modon-new/buruj-updates' element={<BurujUpdates />}/>
+      <Route path='/modon-new/future-updates' element={<FutureUpdates />}/>
+      <Route path='/modon-new/lavida-updates' element={<LavidaUpdates />}/>
+      <Route path='/modon-new/madinaty-updates' element={<MadinatyUpdates />}/>
+      <Route path='/modon-new/bayti-updates' element={<BaytiUpdates />}/>
+      <Route path='/modon-new/commercial' element={<CommercialPage />}/>
+      <Route path='/modon-new/bayti-mall' element={<BaytiMall />}/>
+      <Route path='/modon-new/about-us' element={<AboutPage />}/>
+      <Route path='/modon-new/image-gallery' element={<ImageGallery />}/>
+      <Route path='/modon-new/video-gallery' element={<VideoGallery />}/>
 
     </Routes>
      {/* <Slideshow /> */}
