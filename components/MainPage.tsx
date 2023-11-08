9b1eb4b139c40b669053c83b5c7a38fe16e74c5a
Ali_khalid <aliiikhallid@icloud.com>
2023-11-08 14:52:48 +0200

    Updates

---
+++ components/MainPage.tsx
@@ -0,0 +1,24 @@
+import Header from './sections/Header'
+import ModonProps from './sections/ModonProps'
+import ModonSlider from './sections/ModonSlider'
+import LastPage from './sections/LastPage'
+import Footer from './sections/Footer'
+import SecondPage from './sections/SecondPage'
+import { useLayoutEffect } from 'react'
+const MainPage = () => {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+});
+  return (
+    <>
+     <Header />
+     <SecondPage />
+     <ModonProps />
+     <ModonSlider />
+     <LastPage />
+     <Footer />
+    </>
+  )
+}
+
+export default MainPage
